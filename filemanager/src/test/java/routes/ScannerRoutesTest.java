3ceb583327e0009125feac232097954d8854a998
Christian Castelli <voodoo81people@gmail.com>
2021-12-27 21:42:53 +0100

    ScannerRoute test ok

--- filemanager/src/test/java/routes/ScannerRoutesTest.java
+++ filemanager/src/test/java/routes/ScannerRoutesTest.java
@@ -1,12 +1,21 @@
 package routes;
 
+import models.files.MusicFile;
 import models.files.MusicFileSchema;
 import org.junit.jupiter.api.*;
+import routes.utils.PaginatedResponse;
 import src.DbHelper;
 import src.FileManagerClient;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.net.http.HttpResponse;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -25,8 +34,44 @@ public class ScannerRoutesTest {
     }
 
     @Test @Order(1)
-    public void scanRequestTest() throws URISyntaxException, IOException, InterruptedException {
+    public void scanRequestTest(TestReporter rep) throws URISyntaxException, IOException, InterruptedException {
+        String json = """
+              {
+                "directory": "/home/christian/GitHub/mm/filemanager/src/test/resources/tree"
+              }
+          """;
+        HttpResponse<String> response = client.sendPost("/scanner/scan", json);
+        assertEquals("true", response.body());
 
+        HttpResponse<String> getAllResponse = client.sendGet(
+          "/files/list",
+          new HashMap<>(
+            Map.of(
+              "sort_dir", "asc",
+              "count", "15"
+            )
+          )
+        );
+
+        // The backend doesn't block the client, just answers true and starts to scan
+        PaginatedResponse<MusicFile> pRes = PaginatedResponse.fromJson(getAllResponse.body(), MusicFile.class);
+        List<MusicFile> items = pRes.getItems();
+        assertTrue(items.size() < 5);
+
+        Thread.sleep(1000);
+        
+        getAllResponse = client.sendGet(
+          "/files/list",
+          new HashMap<>(
+            Map.of(
+              "sort_dir", "asc",
+              "count", "15"
+            )
+          )
+        );
+        pRes = PaginatedResponse.fromJson(getAllResponse.body(), MusicFile.class);
+        items = pRes.getItems();
+        assertEquals(14, items.size());
     }
 
 }
